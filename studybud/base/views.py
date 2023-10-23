4fb00b46923746f0638284c6db6ce8f63a0ec88b
leriosted <leriosted@gmail.com>
2023-10-23 17:30:18 +1300

    Changes

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse('Home page')
+    return render(request, 'home.html')
 
 def room(request):
-    return HttpResponse('ROOM')
+    return render(request, 'room.html')
